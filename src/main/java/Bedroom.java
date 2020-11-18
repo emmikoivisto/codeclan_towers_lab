d244f389e3cd46405d530e0d9526aa99a8e8061a
JenkenK <jenkenkwok@gmail.com>
2020-11-18 11:26:44 +0000

    setup hotl and conference room objects

--- src/main/java/Bedroom.java
+++ src/main/java/Bedroom.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+
 public class Bedroom {
+
+    private Integer roomNumber;
+    private Integer capacity;
+    private String roomType;
+    private ArrayList<Guest> guestsInBedroom;
+
+    public Bedroom(int roomNumber, int capacity, String roomType){
+        this.roomNumber = roomNumber;
+        this.capacity = capacity;
+        this.roomType = roomType;
+    }
 }
--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -1,2 +1,4 @@
 public class Hotel {
+    private ArrayList<Guest> guestsInHotel;
+
 }
